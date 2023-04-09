f2df6c622e2360f261e42d513b6b443d573bcb22
andirkoadam <89788115+andirkoadam@users.noreply.github.com>
2023-04-09 14:11:21 +0200

    Add files via upload
    uploading frontend

---
+++ frontend/src/components/Sections/Contact.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import styled from "styled-components";
+
+export default function Contact() {
+  return (
+    <Wrapper id="contact">
+      <div className="lightBg">
+        <div className="container">
+          <HeaderInfo>
+            <h1 className="font40 extraBold">Let's get in touch</h1>
+            <p className="font13">
+              Blablabla
+            </p>
+          </HeaderInfo>
+        </div>
+      </div>
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.section`
+  width: 100%;
+`;
+const HeaderInfo = styled.div`
+  padding: 70px 0 30px 0;
+  @media (max-width: 860px) {
+    text-align: center;
+  }
+`;
+
+
+
+
+
+
+
+
+
---
+++ frontend/src/components/Sections/Services.jsx
@@ -0,0 +1,174 @@
+import React from "react";
+import styled from "styled-components";
+// Components
+import ServiceBox from "../Elements/ServiceBox";
+import FullButton from "../Buttons/FullButton";
+import BrandSlider from "../Elements/BrandSlider";
+// Assets
+import ServiceImg1 from "../../assets/img/services/1.png";
+import ServiceImg2 from "../../assets/img/services/2.png";
+import ServiceImg3 from "../../assets/img/services/3.png";
+import ServiceImg4 from "../../assets/img/services/4.png";
+import ServiceImg5 from "../../assets/img/services/5.png";
+import ServiceImg6 from "../../assets/img/services/6.png";
+
+export default function Services() {
+  return (
+    <Wrapper id="services">
+      <div className="lightBg" style={{ padding: "50px 0" }}>
+        <div className="container">
+          <BrandSlider />
+        </div>
+      </div>
+      <div className="whiteBg">
+        <div className="container">
+          <HeaderInfo>
+            <h1 className="font40 extraBold">Services</h1>
+            <p className="font13">
+              Blablabla
+            </p>
+          </HeaderInfo>
+          <div className="row textCenter">
+            <div className="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+              <ServiceBox
+                img={ServiceImg1}
+                title="Service 1"
+                text="Description of service"
+                action={() => alert("clicked")}
+              />
+            </div>
+            <div className="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+              <ServiceBox
+                img={ServiceImg2}
+                title="Service 2"
+                text="Description of service."
+                action={() => alert("clicked")}
+              />
+            </div>
+            <div className="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+              <ServiceBox
+                img={ServiceImg3}
+                title="Service 3"
+                text="Description of service"
+                action={() => alert("clicked")}
+              />
+            </div>
+          </div>
+          <div className="row textCenter">
+            <div className="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+              <ServiceBox
+                img={ServiceImg4}
+                title="Service 4"
+                text="Description of service"
+                action={() => alert("clicked")}
+              />
+            </div>
+            <div className="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+              <ServiceBox
+                img={ServiceImg5}
+                title="Service 5"
+                text="Description of service"
+                action={() => alert("clicked")}
+              />
+            </div>
+            <div className="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+              <ServiceBox
+                img={ServiceImg6}
+                title="Service 6"
+                text="Description of service"
+                action={() => alert("clicked")}
+              />
+            </div>
+          </div>
+          <div className="row flexCenter">
+            <div style={{ margin: "50px 0", width: "200px" }}>
+              <FullButton title="Load More" action={() => alert("clicked")} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.section`
+  width: 100%;
+`;
+const HeaderInfo = styled.div`
+  @media (max-width: 860px) {
+    text-align: center;
+  }
+`;
+const Advertising = styled.div`
+  padding: 100px 0;
+  margin: 100px 0;
+  position: relative;
+  @media (max-width: 1160px) {
+    padding: 60px 0 40px 0;
+  }
+  @media (max-width: 860px) {
+    flex-direction: column;
+    padding: 0 0 30px 0;
+    margin: 80px 0 0px 0;
+  }
+`;
+const ButtonsRow = styled.div`
+  @media (max-width: 860px) {
+    justify-content: space-between;
+  }
+`;
+const AddLeft = styled.div`
+  position: relative;
+  width: 50%;
+  p {
+    max-width: 475px;
+  }
+  @media (max-width: 860px) {
+    width: 80%;
+    order: 2;
+    text-align: center;
+    h2 {
+      line-height: 3rem;
+      margin: 15px 0;
+    }
+    p {
+      margin: 0 auto;
+    }
+  }
+`;
+const AddRight = styled.div`
+  width: 50%;
+  @media (max-width: 860px) {
+    width: 80%;
+    order: 2;
+  }
+`;
+const AddLeftInner = styled.div`
+  width: 100%;
+  position: absolute;
+  top: -300px;
+  left: 0;
+  @media (max-width: 1190px) {
+    top: -250px;
+  }
+  @media (max-width: 920px) {
+    top: -200px;
+  }
+  @media (max-width: 860px) {
+    order: 1;
+    position: relative;
+    top: -60px;
+    left: 0;
+  }
+`;
+const ImgWrapper = styled.div`
+  width: 100%;
+  padding: 0 15%;
+  img {
+    width: 100%;
+    height: auto;
+  }
+  @media (max-width: 400px) {
+    padding: 0;
+  }
+`;
