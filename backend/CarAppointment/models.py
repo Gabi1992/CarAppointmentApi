9504919c13f8b7fbe41c759567a6928f4bc22ad5
andirkoadam <andirkoadam@gmail.com>
2023-04-19 11:10:43 +0200

    latestcomit

--- backend/CarAppointment/models.py
+++ backend/CarAppointment/models.py
@@ -1,17 +1,5 @@
 from django.db import models
 
-<<<<<<< HEAD
-    
-class megrendelo(models.Model):
-    nev = models.CharField("Adja meg a teljes nevét!",max_length=100)
-    telefonszam = models.CharField("Adja meg a telefonszámot!",max_length=11)
-    email = models.EmailField("Adja meg a email cimét!")
-
-    
-    def __str__(self):
-        return self.nev
-
-=======
 class Car(models.Model):
     brand = models.CharField(max_length= 200)
     year = models.IntegerField()
@@ -19,7 +7,6 @@ class Car(models.Model):
         return self.brand
     
     
->>>>>>> 5657c204ca62905745cda5bd8cf27328c0ba689e
     
     
     
